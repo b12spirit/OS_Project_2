8417bf198fb32d91ab07f5a116c8dde18203f12c
Alonso <alonsovidal1998@gmail.com>
2021-04-23 18:50:16 -0500

    changes

--- calc.c
+++ calc.c
@@ -1,5 +1,6 @@
-/* Project 2 - Multithreaded calculator */
+ /* Project 2 - Multithreaded calculator */
 // Name: Alonso Vidal
+
 
 #include "calc.h"
 
@@ -14,9 +15,12 @@ int num_ops;
 
 /* Step 3: add mutual exclusion */
 
+
 /* Step 6: add condition flag varaibles */
 
+
 /* Step 7: use a semaphore */
+
 
 /* Utiltity functions provided for your convenience */
 
@@ -25,122 +29,137 @@ int num_ops;
    characters).  */
 char *int2string(int i, char *s)
 {
-	sprintf(s, "%d", i);
-	return s;
+    sprintf(s, "%d", i);
+    return s;
 }
 
 /* string2int just calls atoi() */
 int string2int(const char *s)
 {
-	return atoi(s);
+    return atoi(s);
 }
 
 /* isNumeric just calls isdigit() */
 int isNumeric(char c)
 {
-	return isdigit(c);
+    return isdigit(c);
 }
 
 /* End utility functions */
 
+
 void printErrorAndExit(char *msg)
 {
-	msg = msg ? msg : "An unspecified error occured!";
-	fprintf(stderr, "%s\n", msg);
-	exit(EXIT_FAILURE);
+    msg = msg ? msg : "An unspecified error occured!";
+    fprintf(stderr, "%s\n", msg);
+    exit(EXIT_FAILURE);
 }
 
 int timeToFinish()
 {
-	/* be careful: timeToFinish() also accesses buffer */
-	return buffer[0] == '.';
+    /* be careful: timeToFinish() also accesses buffer */
+    return buffer[0] == '.';
 }
 
 /* Looks for an addition symbol "+" surrounded by two numbers, e.g. "5+6"
    and, if found, adds the two numbers and replaces the addition subexpression 
    with the result ("(5+6)*8" becomes "(11)*8")--remember, you don't have
    to worry about associativity! */
-/* As requested, the adder function alone has been implemented */
-
-/* Looks for an addition symbol "+" surrounded by two numbers, e.g. "5+6"
-and, if found, adds the two numbers and replaces the addition subexpression
-with the result ("(5+6)*8" becomes "(11)*8")--remember, you don't have
-to worry about associativity! */
 void *adder(void *arg)
 {
-	int bufferlen;
-	int value1, value2;
-	int startOffset, remainderOffset;
-	int i, sum, operlength;
-	char *operand;
+    int bufferlen;
+    int value1, value2;
+    int startOffset, remainderOffset;
+    int i;
 
-	while (1)
-	{
-		startOffset = remainderOffset = -1;
-		value1 = value2 = -1;
-		sum = 0;
+    char numstring[50];
+    int num = 0;
+    
+    
+    while (1) {
+        if(progress.add != 2){
 
-		if (timeToFinish())
-		{
-			return NULL;
-		}
+    
+		/* Step 3: add mutual exclusion */
+		pthread_mutex_lock(&buffer_lock);
+	//printf("Adder Thread active: %s\n",buffer);	
+	startOffset = remainderOffset = -1;
+	value1 = value2 = -1;
 
-		/* storing this prevents having to recalculate it in the loop */
-		bufferlen = strlen(buffer);
-		operand = (char *)malloc(bufflen * sizeof(char));
-
-		for (i = 0; i < bufferlen; i++)
-		{
-			// Look for a '+' in the expression.
-			if (buffer[i] == '+')
-			{
-				// Look for the beginning of the left operand
-				for (startOffset = i; startOffset - 1 >= 0 && isNumeric(buffer[startOffset - 1]); --startOffset)
-					;
-				// If the left operand is not a naked number, proceed with finding the next '+'
-				if (startOffset == i)
-					continue;
-
-				// Look for the beginning of the right operand
-				for (remainderOffset = i; remainderOffset + 1 < bufferlen && isNumeric(buffer[remainderOffset + 1]); ++remainderOffset)
-					;
-				// If the right operand is not a naked number, proceed with finding the next '+'
-				if (remainderOffset == i)
-					continue;
-
-				// Now we have a naked number on both the left and right sides
-				// Let us now convert the operands to numbers
-
-				// Left operand
-				strncpy(operand, &buffer[startOffset], i - startOffset);
-				operand[i - startOffset] = '\0';
-				string2int(value1, operand);
-
-				// Right operand
-				strncpy(operand, &buffer[remainderOffset], remainderOffset - i);
-				operand[remainderOffset - i] = '\0';
-				string2int(value2, operand);
-
-				// add the two operands
-				sum = value1 + value2;
-
-				// convert the numeric sum to a string
-				sprint(operand, "%d", sum);
-
-				operlength = strlen(operand);
-				// write the string sum back into the buffer
-				strncpy(&buffer[startOffset], operand, operlength);
-
-				// Shift the remaining characters to the left
-				strcpy(&buffer[operlength], &buffer[remainderOffset + 1]);
-
-				// Accordingly reset the buffer length
-				bufferlen = bufferlen - (remainderOffset - startOffset + 1) + operlength;
-			}
-		}
-		free(operand);
+	if (timeToFinish()) {
+	    pthread_mutex_unlock(&buffer_lock);
+	    return NULL;
 	}
-}
+
+	/* storing this prevents having to recalculate it in the loop */
+	bufferlen = strlen(buffer);
+
+	/* Step 2: implement adder */
+	for (i = 0; i < bufferlen; i++) {
+	    // do we have value1 already?  If not, is this a "naked" number?
+	    // if we do, is the next character after it a '+'?
+	    // if so, is the next one after the sign a "naked" number?
+        
+         if(isNumeric(buffer[i])) //copypasted from adder
+        {
+            startOffset = i;
+            
+            value1 = atoi(&buffer[i]);
+              //printf(value1);
+              
+            while (isdigit(buffer[i]))
+            {
+                i++;
+            }
+            
+            if(buffer[i] != '+' || !isNumeric(buffer[i+1]))
+            {continue;}
+            value2 = string2int(&buffer[i+1]);
+            int result = value1 + value2;
+              //printf("%s\n", buffer);
+            do{
+                i++;
+              }
+            while (isNumeric(buffer[i]));
+            remainderOffset = i;
+            
+            int2string(result,nstring);
+            
+            //printf("Changed Started at I = %d",i);
+            char BufferBackup[500];
+            strcpy(BufferBackup,&buffer);
+            
+            //printf("%s\n", buffer);
+            strcpy(buffer + startOffset, &nstring);
+            //printf("%s\n", buffer);
+            strcpy(buffer + (startOffset + strlen(nstring)), &BufferBackup[remainderOffset]);
+            //printf("%s\n", buffer);
+            bufferlen = bufferlen - (remainderOffset - 1 - startOffset);//5(35+5)-6  < So: 2 rm:6
+            i = startOffset-1; ///2+4*6*9 [i=6] > 2+24*9 [i=3 *] > 
+            
+            changed = 1;
+            num_ops++;
+        }
+        
+	    // once we have value1, value2 and start and end offsets of the
+	    // expression in buffer, replace it with v1+v2
+	}
+
+	// something missing?
+	/* Step 3: free the lock */
+    pthread_mutex_unlock(&buffer_lock);
+//printf("Adder Thread Unlocked\n");
+	/* Step 6: check progress */
+	//	printf("adder Waiting for progress\n");
+    sem_wait(&progress_lock);
+    progress.add = changed ? 1 : 2; // 1 means changed, 2 didn't changed;
+    sem_post(&progress_lock);
+    //	printf("adder posted progress\n");
+    changed = 0;
+	/* Step 5: let others play */
+	sched_yield();
+    }else{sched_yield();}
+}}
 
 /* Looks for a multiplication symbol "*" surrounded by two numbers, e.g.
    "5*6" and, if found, multiplies the two numbers and replaces the
@@ -148,82 +167,86 @@ void *adder(void *arg)
    "1+(30)+8"). */
 void *multiplier(void *arg)
 {
-	int bufferlen;
-	int value1, value2;
-	int startOffset, remainderOffset;
-	int i;
+    int bufferlen;
+    int value1, value2;
+    int startOffset, remainderOffset;
+    int i;
 
-	return NULL; /* remove this line */
+    return NULL; /* remove this line */
 
-	while (1)
-	{
+    while (1) {
 		/* Step 3: add mutual exclusion */
 
-		startOffset = remainderOffset = -1;
-		value1 = value2 = -1;
 
-		if (timeToFinish())
-		{
-			return NULL;
-		}
+	startOffset = remainderOffset = -1;
+	value1 = value2 = -1;
 
-		/* storing this prevents having to recalculate it in the loop */
-		bufferlen = strlen(buffer);
-
-		/* Step 2: implement multiplier */
-		for (i = 0; i < bufferlen; i++)
-		{
-			// same as adder, but v1*v2
-		}
-
-		// something missing?
-		/* Step 3: free the lock */
-
-		/* Step 6: check progress */
-
-		/* Step 5: let others play */
+	if (timeToFinish()) {
+	    return NULL;
 	}
+
+	/* storing this prevents having to recalculate it in the loop */
+	bufferlen = strlen(buffer);
+
+	/* Step 2: implement multiplier */
+	for (i = 0; i < bufferlen; i++) {
+	    // same as adder, but v1*v2
+	}
+
+	// something missing?
+	/* Step 3: free the lock */
+	
+
+	/* Step 6: check progress */
+
+
+	/* Step 5: let others play */
+
+    }
 }
+
 
 /* Looks for a number immediately surrounded by parentheses [e.g.
    "(56)"] in the buffer and, if found, removes the parentheses leaving
    only the surrounded number. */
 void *degrouper(void *arg)
 {
-	int bufferlen;
-	int i;
+    int bufferlen;
+    int i;
 
-	return NULL; /* remove this line */
+    return NULL; /* remove this line */
 
-	while (1)
-	{
+    while (1) {
 
 		/* Step 3: add mutual exclusion */
 
-		if (timeToFinish())
-		{
-			return NULL;
-		}
 
-		/* storing this prevents having to recalculate it in the loop */
-		bufferlen = strlen(buffer);
-
-		/* Step 2: implement degrouper */
-		for (i = 0; i < bufferlen; i++)
-		{
-			// check for '(' followed by a naked number followed by ')'
-			// remove ')' by shifting the tail end of the expression
-			// remove '(' by shifting the beginning of the expression
-		}
-
-		// something missing?
-		/* Step 3: free the lock */
-
-		/* Step 6: check progress */
-
-		/* Step 5: let others play */
+	if (timeToFinish()) {
+	    return NULL;
 	}
+
+	/* storing this prevents having to recalculate it in the loop */
+	bufferlen = strlen(buffer);
+
+	/* Step 2: implement degrouper */
+	for (i = 0; i < bufferlen; i++) {
+	    // check for '(' followed by a naked number followed by ')'
+	    // remove ')' by shifting the tail end of the expression
+	    // remove '(' by shifting the beginning of the expression
+	}
+
+	// something missing?
+	/* Step 3: free the lock */
+
+
+	/* Step 6: check progress */
+
+
+	/* Step 5: let others play */
+
+    }
 }
+
 
 /* sentinel waits for a number followed by a ; (e.g. "453;") to appear
    at the beginning of the buffer, indicating that the current
@@ -232,142 +255,134 @@ void *degrouper(void *arg)
    proceed on the next (if available). */
 void *sentinel(void *arg)
 {
-	char numberBuffer[20];
-	int bufferlen;
-	int i;
+    char numberBuffer[20];
+    int bufferlen;
+    int i;
 
-	return NULL; /* remove this line */
+    return NULL; /* remove this line */
 
-	while (1)
-	{
+    while (1) {
 
 		/* Step 3: add mutual exclusion */
 
-		if (timeToFinish())
-		{
-			return NULL;
-		}
-
-		/* storing this prevents having to recalculate it in the loop */
-		bufferlen = strlen(buffer);
-
-		for (i = 0; i < bufferlen; i++)
-		{
-			if (buffer[i] == ';')
-			{
-				if (i == 0)
-				{
-					printErrorAndExit("Sentinel found empty expression!");
-				}
-				else
-				{
-					/* null terminate the string */
-					numberBuffer[i] = '\0';
-					/* print out the number we've found */
-					fprintf(stdout, "%s\n", numberBuffer);
-					/* shift the remainder of the string to the left */
-					strcpy(buffer, &buffer[i + 1]);
-					break;
-				}
-			}
-			else if (!isNumeric(buffer[i]))
-			{
-				break;
-			}
-			else
-			{
-				numberBuffer[i] = buffer[i];
-			}
-		}
-
-		// something missing?
-		/* Step 6: check for progress */
-
-		/* Step 5: let others play, too */
+	if (timeToFinish()) {
+	    return NULL;
 	}
+
+	/* storing this prevents having to recalculate it in the loop */
+	bufferlen = strlen(buffer);
+
+	for (i = 0; i < bufferlen; i++) {
+	    if (buffer[i] == ';') {
+		if (i == 0) {
+		    printErrorAndExit("Sentinel found empty expression!");
+		} else {
+		    /* null terminate the string */
+		    numberBuffer[i] = '\0';
+		    /* print out the number we've found */
+		    fprintf(stdout, "%s\n", numberBuffer);
+		    /* shift the remainder of the string to the left */
+		    strcpy(buffer, &buffer[i + 1]);
+		    break;
+		}
+	    } else if (!isNumeric(buffer[i])) {
+		break;
+	    } else {
+		numberBuffer[i] = buffer[i];
+	    }
+	}
+
+	// something missing?
+	/* Step 6: check for progress */
+
+
+	/* Step 5: let others play, too */
+
+    }
 }
 
 /* reader reads in lines of input from stdin and writes them to the
    buffer */
 void *reader(void *arg)
 {
-	while (1)
-	{
-		char tBuffer[100];
-		int currentlen;
-		int newlen;
-		int free;
+    while (1) {
+	char tBuffer[100];
+	int currentlen;
+	int newlen;
+	int free;
 
-		fgets(tBuffer, sizeof(tBuffer), stdin);
+	fgets(tBuffer, sizeof(tBuffer), stdin);
 
-		/* Sychronization bugs in remainder of function need to be fixed */
+	/* Sychronization bugs in remainder of function need to be fixed */
 
-		newlen = strlen(tBuffer);
-		currentlen = strlen(buffer);
+	newlen = strlen(tBuffer);
+	currentlen = strlen(buffer);
 
-		/* if tBuffer comes back with a newline from fgets, remove it */
-		if (tBuffer[newlen - 1] == '\n')
-		{
-			/* shift null terminator left */
-			tBuffer[newlen - 1] = tBuffer[newlen];
-			newlen--;
-		}
-
-		/* -1 for null terminator, -1 for ; separator */
-		free = sizeof(buffer) - currentlen - 2;
-
-		while (free < newlen)
-		{
-			// spinwaiting TO DO
-		}
-
-		/* Step 3: add mutual exclusion */
-
-		/* we can add another expression now */
-		strcat(buffer, tBuffer);
-		strcat(buffer, ";");
-
-		/* Step 6: reset flag variables indicating progress */
-
-		/* Stop when user enters '.' */
-		if (tBuffer[0] == '.')
-		{
-			return NULL;
-		}
+	/* if tBuffer comes back with a newline from fgets, remove it */
+	if (tBuffer[newlen - 1] == '\n') {
+	    /* shift null terminator left */
+	    tBuffer[newlen - 1] = tBuffer[newlen];
+	    newlen--;
 	}
+
+	/* -1 for null terminator, -1 for ; separator */
+	free = sizeof(buffer) - currentlen - 2;
+
+	while (free < newlen) {
+		// spinwaiting TO DO
+	}
+
+	/* Step 3: add mutual exclusion */
+
+	/* we can add another expression now */
+	strcat(buffer, tBuffer);
+	strcat(buffer, ";");
+
+	/* Step 6: reset flag variables indicating progress */
+
+	/* Stop when user enters '.' */
+	if (tBuffer[0] == '.') {
+	    return NULL;
+	}
+    }
 }
+
 
 /* Where it all begins */
 int smp3_main(int argc, char **argv)
 {
-	void *arg = 0; /* dummy value */
+    void *arg = 0;		/* dummy value */
 
 	/* Step 7: initialize your semaphore */
 
-	/* let's create our threads */
-	if (pthread_create(&multiplierThread, NULL, multiplier, arg) || pthread_create(&adderThread, NULL, adder, arg) || pthread_create(&degrouperThread, NULL, degrouper, arg) || pthread_create(&sentinelThread, NULL, sentinel, arg) || pthread_create(&readerThread, NULL, reader, arg))
-	{
-		printErrorAndExit("Failed trying to create threads");
-	}
+    /* let's create our threads */
+    if (pthread_create(&multiplierThread, NULL, multiplier, arg)
+	|| pthread_create(&adderThread, NULL, adder, arg)
+	|| pthread_create(&degrouperThread, NULL, degrouper, arg)
+	|| pthread_create(&sentinelThread, NULL, sentinel, arg)
+	|| pthread_create(&readerThread, NULL, reader, arg)) {
+	printErrorAndExit("Failed trying to create threads");
+    }
 
-	/* you need to join one of these threads... but which one? */
+    /* you need to join one of these threads... but which one? */
 	/* The pthread_join() function for threads is the equivalent of wait() 
 	for processes. A call to pthread_join blocks the calling thread until 
 	the thread with identifier equal to the first argument terminates.*/
 	// If you join do not detach.
-	pthread_detach(multiplierThread);
-	pthread_detach(adderThread);
-	pthread_detach(degrouperThread);
-	pthread_detach(sentinelThread);
-	pthread_detach(readerThread);
+    pthread_detach(multiplierThread);
+    pthread_detach(adderThread);
+    pthread_detach(degrouperThread);
+    pthread_detach(sentinelThread);
+    pthread_detach(readerThread);
 	/* Step 1: we have to join on the ________ thread. */
 	// pthread_join(____, NULL);
 
-	/* everything is finished, print out the number of operations performed */
-	fprintf(stdout, "Performed a total of %d operations\n", num_ops);
+    /* everything is finished, print out the number of operations performed */
+    fprintf(stdout, "Performed a total of %d operations\n", num_ops);
 
 	// TODO destroy semaphores and mutex
 
+
 	// return
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
